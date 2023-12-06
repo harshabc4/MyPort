2d0a9702cf5ca3f36ba929a93cb5e37a0ea77920
Brian Harsha <harshabc4@gmail.com>
2023-12-06 17:16:27 -0500

    test deploy

---
+++ client/src/components/Box/Box.tsx
@@ -0,0 +1,49 @@
+import { FC, useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface BoxProps { }
+interface Feed {
+   _id: string;
+   text: string;
+   __v: number;
+ }
+
+const Box: FC<BoxProps> = () => {
+   const [feeds, setFeedsData] = useState<Feed | null>(null);
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            var endpoint = "/api/getPosts";
+            console.log(endpoint);
+            // console.log(endpoint);
+            const response = await axios
+               // .get("/showMaterials")
+               // .get("/getPosts")
+               .get<Feed>(endpoint)
+               // .then((res) => setFeedsData(res))
+               .then((res) => {
+                  console.log(res.data);
+                  // console.log("this is materials from appjs frontend socialmedia");
+                  // return res.data;
+                  setFeedsData(res.data);
+               });
+               console.log(response);
+            // console.log(response);
+         } catch (err) {
+            console.error(err);
+         }
+      };
+      fetchData();
+   }, []);
+   return (
+      <div>
+         <div>adsf</div>
+         {feeds ? (
+        feeds.text) : (
+        <div>No data available</div>
+      )}
+      </div>
+   );
+};
+
+export default Box;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:2121',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   }
 })
