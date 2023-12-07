c6685d6bfbef2667b043ca233aeb7c2a95b0d59a
Brian Harsha <harshabc4@gmail.com>
2023-12-06 19:08:06 -0500

    yes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: '../index.js',
+      input: '/index.js',
     },
   },
 })
