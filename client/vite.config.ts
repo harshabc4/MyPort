01121b2afd3646de2782be2b55f8d029271555fc
Brian Harsha <harshabc4@gmail.com>
2023-12-06 19:12:29 -0500

    remove biuld stuff

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,12 +14,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/index.js',
-    },
-  },
 })
